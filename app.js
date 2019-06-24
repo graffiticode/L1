d0a889f48274d6254f7c053689bf82d6f9b5c319
Kelton Manzanares <keltonmanzanares@gmail.com>
2019-06-24 13:43:58 -0500

    adjusting static file path

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ exports.createApp = (auth, compiler) => {
     console.error(err.stack);
     res.sendStatus(500);
   });
-  app.use(express.static(path.resolve(__dirname, './../pub')));
+  app.use(express.static(path.resolve(__dirname, './pub')));
   app.get('/', routes.lang(compiler));
   app.get('/version', routes.version(compiler));
   app.get('/compile', routes.auth(auth, 'compile'), routes.compile(compiler));
