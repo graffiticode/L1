490df82e790d19dc25c5fe7bcea10749c63c661b
Kevin Dyer <kevindy@google.com>
2019-10-04 15:00:27 +0200

    Removed unused routes

--- routes/root.js
+++
@@ -1,6 +0,0 @@
-module.exports = (compiler) => {
-  return (req, res) => {
-    const langID = compiler.langID || '0';
-    res.send(`Hello, L${langID}!`);
-  };
-};
--- routes/version.js
+++
@@ -1,5 +0,0 @@
-module.exports = (compiler) => {
-  return (req, res) => {
-    res.send(compiler.version || "v0.0.0");
-  };
-};
