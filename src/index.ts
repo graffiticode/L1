c2a34826af002eac458b8fc1d703da8460d01180
Kevin Dyer <kevindy@google.com>
2019-10-13 16:04:39 +0200

    Converted app.js to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
-console.log("Try npm run check/fix!");
+import { AuthError } from '@graffiticode/graffiticode-compiler-framework';
+import { createAuth } from './auth';
+import { compile, langID } from './compile';
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const language = `L${langID}`;
+const auth = createAuth(language);
 
-const trailing = 'Semicolon'
-
-            const why = 'am I tabbed?';
-
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!andThose.length) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-}
-// TODO: more examples
+export const compiler = {
+  language,
+  auth(token: string) {
+    return new Promise((resolve, reject) => {
+      auth(token, 'compile', (err, authInfo) => {
+        if (err) {
+          reject(new AuthError(err.message));
+        } else {
+          resolve(authInfo);
+        }
+      });
+    });
+  },
+  // tslint:disable-next-line:no-any
+  compile(code: any, data: any, config: any) {
+    return new Promise((resolve, reject) => {
+      compile(code, data, config, (err, result) => {
+        if (err instanceof Error || (Array.isArray(err) && err.length > 0)) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  },
+};
